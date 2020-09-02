30c36692eb49d248e2cdb4ff4c3081d900078771
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-09-02 17:09:54 +0200

    [REF] SEOS-1691: functional decomposition of OS_CertParserCert.c::OS_CertParserCert_init()

--- src/OS_CertParserCert.c
+++ src/OS_CertParserCert.c
@@ -9,6 +9,72 @@
 #include <stdint.h>
 #include <string.h>
 
+// Private functions -----------------------------------------------------------
+static inline OS_Error_t
+initImpl(
+    OS_CertParserCert_t* cert,
+    const OS_CertParserCert_Encoding_t encoding)
+{
+    OS_Error_t retval = OS_ERROR_GENERIC;
+    // Also, we parse the cert data into an mbedTLS structure so we can check if
+    // the provided format is valid and to make extraction of attribues easy.
+    mbedtls_x509_crt_init(&cert->mbedtls.cert);
+
+    // Translate parsing errors due to unkown algos into NOT_SUPPORTED, so it
+    // aligns with the follow-up check. UNKNOWN_SIG_ALG will be returned in cases
+    // where mbedTLS has not been compiled for a certain algorithm. In our case,
+    // mbedTLS may actually provide more algorithms than we want to have as per
+    // the certProfile, so that is why we add a second check below.
+    switch (encoding)
+    {
+        int rc;
+
+    case OS_CertParserCert_Encoding_DER:
+        if ((rc = mbedtls_x509_crt_parse_der(&cert->mbedtls.cert,
+                                             cert->data,
+                                             cert->len)) != 0)
+        {
+            Debug_LOG_RET_MBEDTLS("mbedtls_x509_crt_parse_der", rc);
+            retval = (rc & MBEDTLS_ERR_X509_UNKNOWN_SIG_ALG) ?
+                     OS_ERROR_NOT_SUPPORTED : OS_ERROR_ABORTED;
+            goto err;
+        }
+        break;
+    case OS_CertParserCert_Encoding_PEM:
+        if ((rc = mbedtls_x509_crt_parse(&cert->mbedtls.cert,
+                                         cert->data,
+                                         cert->len)) != 0)
+        {
+            Debug_LOG_RET_MBEDTLS("mbedtls_x509_crt_parse", rc);
+            retval = (rc & MBEDTLS_ERR_X509_UNKNOWN_SIG_ALG) ?
+                     OS_ERROR_NOT_SUPPORTED : OS_ERROR_ABORTED;
+            goto err;
+        }
+        break;
+    default:
+        retval = OS_ERROR_INVALID_PARAMETER;
+        goto err;
+    }
+
+    // The certificate verification checks if a cert has uses only allowed
+    // algorithms for hash and PK. However, to make user experience more
+    // consistent, we do these checks here already so a user cannot even create
+    // a cert that will later fail.
+    if ( !(certProfile.allowed_mds & MBEDTLS_X509_ID_FLAG(
+               cert->mbedtls.cert.sig_md))
+         || !(certProfile.allowed_pks & MBEDTLS_X509_ID_FLAG(
+                  cert->mbedtls.cert.sig_pk)) )
+    {
+        Debug_LOG_ERROR("Certificate PK or HASH algorithm are not supported");
+        retval = OS_ERROR_NOT_SUPPORTED;
+        goto err;
+    }
+    return OS_SUCCESS;
+err:
+    mbedtls_x509_crt_free(&cert->mbedtls.cert);
+    return retval;
+}
+
 // Public functions ------------------------------------------------------------
 
 OS_Error_t
@@ -19,7 +85,6 @@ OS_CertParserCert_init(
     const uint8_t*                     data,
     const size_t                       len)
 {
-    int rc;
     OS_Error_t err;
     OS_CertParserCert_t* cert;
 
@@ -39,7 +104,7 @@ OS_CertParserCert_init(
     cert->len = (data[len - 1] == 0x00) ? len : len + 1;
     if ((cert->data = calloc(cert->len, sizeof(uint8_t))) == NULL)
     {
-        rc = OS_ERROR_INSUFFICIENT_SPACE;
+        err  = OS_ERROR_INSUFFICIENT_SPACE;
         goto err0;
     }
 
@@ -48,68 +113,21 @@ OS_CertParserCert_init(
     memcpy(cert->data, data, len);
     cert->encoding = encoding;
 
-    // Also, we parse the cert data into an mbedTLS structure so we can check if
-    // the provided format is valid and to make extraction of attribues easy.
-    mbedtls_x509_crt_init(&cert->mbedtls.cert);
-
-    // Translate parsing errors due to unkown algos into NOT_SUPPORTED, so it
-    // aligns with the follow-up check. UNKNOWN_SIG_ALG will be returned in cases
-    // where mbedTLS has not been compiled for a certain algorithm. In our case,
-    // mbedTLS may actually provide more algorithms than we want to have as per
-    // the certProfile, so that is why we add a second check below.
-    switch (encoding)
+    err = initImpl(cert, encoding);
+    if (OS_SUCCESS != err)
     {
-    case OS_CertParserCert_Encoding_DER:
-        if ((rc = mbedtls_x509_crt_parse_der(&cert->mbedtls.cert,
-                                             cert->data,
-                                             cert->len)) != 0)
-        {
-            Debug_LOG_RET_MBEDTLS("mbedtls_x509_crt_parse_der", rc);
-            err = (rc & MBEDTLS_ERR_X509_UNKNOWN_SIG_ALG) ?
-                  OS_ERROR_NOT_SUPPORTED : OS_ERROR_ABORTED;
-            goto err1;
-        }
-        break;
-    case OS_CertParserCert_Encoding_PEM:
-        if ((rc = mbedtls_x509_crt_parse(&cert->mbedtls.cert,
-                                         cert->data,
-                                         cert->len)) != 0)
-        {
-            Debug_LOG_RET_MBEDTLS("mbedtls_x509_crt_parse", rc);
-            err = (rc & MBEDTLS_ERR_X509_UNKNOWN_SIG_ALG) ?
-                  OS_ERROR_NOT_SUPPORTED : OS_ERROR_ABORTED;
-            goto err1;
-        }
-        break;
-    default:
-        err = OS_ERROR_INVALID_PARAMETER;
         goto err1;
     }
-
-    // The certificate verification checks if a cert has uses only allowed
-    // algorithms for hash and PK. However, to make user experience more
-    // consistent, we do these checks here already so a user cannot even create
-    // a cert that will later fail.
-    if ( !(certProfile.allowed_mds & MBEDTLS_X509_ID_FLAG(
-               cert->mbedtls.cert.sig_md))
-         || !(certProfile.allowed_pks & MBEDTLS_X509_ID_FLAG(
-                  cert->mbedtls.cert.sig_pk)) )
+    else
     {
-        Debug_LOG_ERROR("Certificate PK or HASH algorithm are not supported");
-        err = OS_ERROR_NOT_SUPPORTED;
-        goto err1;
+        *self = cert;
     }
-
-    *self = cert;
-
     return OS_SUCCESS;
 
 err1:
-    mbedtls_x509_crt_free(&cert->mbedtls.cert);
     free(cert->data);
 err0:
     free(cert);
-
     return err;
 }
 
